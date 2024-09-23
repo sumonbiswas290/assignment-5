92b6c7ee930915aec93723a8823a05ee7b2f79e0
Sumon Biswas <exasumon290@gmail.com>
2024-09-24 00:14:58 +0600

    design added

--- utility.js
+++ utility.js
@@ -1,1 +1,17 @@
-console.log("Yes I'm hare. :)");
+
+function alart() {
+    alert("Hello! I am an alert box!");
+  }
+
+document.getElementById('history-btn').addEventListener('click', function(){
+    alart()
+    document.getElementById('donation').setAttribute('hidden', true)
+    document.getElementById('history').removeAttribute('hidden')
+
+})
+
+document.getElementById('donation-btn').addEventListener('click', function(){
+    document.getElementById('history').setAttribute('hidden', true)
+    document.getElementById('donation').removeAttribute('hidden')
+
+})
